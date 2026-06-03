2d793fb360644607929cd27e164226eb251f6cb9
marchiaw <marisyaauliaputri@gmail.com>
2026-06-03 13:38:19 +0700

    Create database.py

---
+++ database.py
@@ -0,0 +1,30 @@
+import json
+import os
+from structures import WarungMakan
+
+FILE_DB = "database_makanan.json"  
+FILE_FAV = "favorit.json"
+FILE_HIST = "riwayat.json"
+
+def simpan_semua_data(database_warung, daftar_favorit, riwayat_cari):
+    data_bisa_disimpan = []
+    for item in database_warung:
+        data_bisa_disimpan.append({
+            "nama": item.nama,
+            "harga": item.harga,
+            "kategori": item.kategori,
+            "nama_warung": item.nama_warung,
+            "lokasi": item.lokasi,
+            "rating": item.rating
+        })
+    with open(FILE_DB, "w", encoding="utf-8") as f:
+        json.dump(data_bisa_disimpan, f, indent=4)
+
+    with open(FILE_FAV, "w", encoding="utf-8") as f:
+        json.dump(daftar_favorit.get_all(), f, indent=4)
+
+    with open(FILE_HIST, "w", encoding="utf-8") as f:
+        json.dump(riwayat_cari.get_all()[::-1], f, indent=4)
+
+def load_semua_data(riwayat_cari, daftar_favorit):
+    database_warung = []
