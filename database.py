d169f5234d14198bd13200f23eea992f5dd56630
marchiaw <marisyaauliaputri@gmail.com>
2026-06-03 23:38:16 +0700

    Update database.py

--- database.py
+++ database.py
@@ -17,3 +17,11 @@ def simpan_semua_state(database_warung, daftar_favorit, riwayat_cari):
             "lokasi": item.lokasi,
             "rating": item.rating
         })
+   with open(FILE_DB, "w", encoding="utf-8") as f:
+        json.dump(data_bisa_disimpan, f, indent=4)
+
+    with open(FILE_FAV, "w", encoding="utf-8") as f:
+        json.dump(daftar_favorit.get_all(), f, indent=4)
+
+    with open(FILE_HIST, "w", encoding="utf-8") as f:
+        json.dump(riwayat_cari.get_all()[::-1], f, indent=4)
