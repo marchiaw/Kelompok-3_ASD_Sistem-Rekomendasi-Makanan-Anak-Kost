af10c4771ce551efde4844035fc455ed07571481
ronaputri <putrikisamoga@gmail.com>
2026-06-03 23:57:23 +0700

    Update structures.py

--- structures.py
+++ structures.py
@@ -102,3 +102,15 @@ class BSTHarga:
             self.root = NodeBST(warung_obj)
         else:
             self._insert_recursive(self.root, warung_obj)
+
+    def _insert_recursive(self, current, warung_obj):
+        if warung_obj.harga < current.warung.harga:
+            if current.left is None:
+                current.left = NodeBST(warung_obj)
+            else:
+                self._insert_recursive(current.left, warung_obj)
+        else:
+            if current.right is None:
+                current.right = NodeBST(warung_obj)
+            else:
+                self._insert_recursive(current.right, warung_obj)
