7e1fca4d501e77e4ef4c6b6291e50a89e8069e19
ronaputri <putrikisamoga@gmail.com>
2026-06-03 13:53:10 +0700

    QueueHistoryLinkedList

--- structures.py
+++ structures.py
@@ -15,3 +15,37 @@ class NodeLL:
     def __init__(self, data):
         self.data = data
         self.next = None
+
+class QueueHistoryLinkedList:
+    def __init__(self):
+        self.front = None
+        self.rear = None
+        self.size = 0
+
+    def add_history_queue(self, item_text):
+        new_node = NodeLL(item_text)
+        if self.rear is None:
+            self.front = self.rear = new_node
+            self.size = 1
+            return
+        self.rear.next = new_node
+        self.rear = new_node
+        self.size += 1
+        if self.size > 5:
+            self.dequeue_oldest()
+
+    def dequeue_oldest(self):
+        if self.front is None:
+            return
+        self.front = self.front.next
+        if self.front is None:
+            self.rear = None
+        self.size -= 1
+
+    def get_all(self):
+        result = []
+        current = self.front
+        while current:
+            result.append(current.data)
+            current = current.next
+        return result[::-1]
