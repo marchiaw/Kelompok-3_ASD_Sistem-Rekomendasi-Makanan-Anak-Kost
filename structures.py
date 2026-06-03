88c2b730457bd93b79e72a25cc15a8f22f0dcd26
ronaputri <putrikisamoga@gmail.com>
2026-06-03 23:33:24 +0700

    Update structures.py

--- structures.py
+++ structures.py
@@ -2,3 +2,21 @@ class NodeLL:
     def __init__(self, data):
         self.data = data
         self.next = None
+
+class QueueHistoryLinkedList:
+    def __init__(self):
+        self.front = None
+        self.rear = None
+        self.size = 0
+
+    def add_history_queue(self, item_text):
+        new_node = NodeLL(item_text)
+        if self.rear is None:
+            self.front = self.rear = new_node
+            self.size = 1
+            return
+        self.rear.next = new_node
+        self.rear = new_node
+        self.size += 1
+        if self.size > 5:
+            self.dequeue_oldest()
