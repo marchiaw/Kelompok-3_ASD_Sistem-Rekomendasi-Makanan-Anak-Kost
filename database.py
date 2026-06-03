11fa091f04f05ad5192396ad42b530f4ecc3a924
marchiaw <marisyaauliaputri@gmail.com>
2026-06-03 23:43:51 +0700

    Update database.py

--- database.py
+++ database.py
@@ -25,3 +25,19 @@ def simpan_semua_state(database_warung, daftar_favorit, riwayat_cari):
 
     with open(FILE_HIST, "w", encoding="utf-8") as f:
         json.dump(riwayat_cari.get_all()[::-1], f, indent=4)
+        
+def load_semua_state(riwayat_cari, daftar_favorit):
+    database_warung = []
+    if os.path.exists(FILE_DB):
+        try:
+            with open(FILE_DB, "r", encoding="utf-8") as f:
+                data_mentah = json.load(f)
+                for item in data_mentah:
+                    database_warung.append(
+                        WarungMakan(item["nama"], item["harga"], item["kategori"], 
+                                    item["nama_warung"], item["lokasi"], item["rating"])
+                    )
+        except json.JSONDecodeError:
+            database_warung = suntik_data_default()
+    else:
+        database_warung = suntik_data_default()
