2619cb92f7e987c3f2c1f48f3689e55d3cae2be6
Nayla Syahira Adi Putri <naylasyahira2017@gmail.com>
2026-06-04 09:45:39 +0700

    Add nama_warung attribute to WarungMakan class

--- models.py
+++ models.py
@@ -7,3 +7,4 @@ class Kuliner:
 class WarungMakan(Kuliner):
     def __init__(self, nama, harga, kategori, nama_warung, lokasi, rating):
         super().__init__(nama, harga, kategori)
+        self.nama_warung = nama_warung
