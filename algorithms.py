43eb6d5339c86e37f3207c4d30c018a371266898
Nayla Syahira Adi Putri <naylasyahira2017@gmail.com>
2026-06-04 09:36:51 +0700

    Implement binary search for warung names

--- algorithms.py
+++ algorithms.py
@@ -2,3 +2,13 @@ def binary_search_warung(arr, target_nama):
     low = 0
     high = len(arr) - 1
     target = target_nama.lower()
+
+    while low <= high:
+        mid = (low + high) // 2
+        if target == arr[mid].nama_warung.lower():
+            return mid
+        elif arr[mid].nama_warung.lower() < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
